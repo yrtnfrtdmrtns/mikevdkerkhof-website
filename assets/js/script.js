f3dfd9070a934021d1d5434c5a235186803fb480
Ayrton <ayrtonfurtadomartins@gmail.com>
2021-02-02 18:51:43 +0100

    Added animations and elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,63 @@
 $(document).ready(function() {
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+  $('.scroll-down').click(function() {
+    var slider = $('#slider').offset().top - 77;
+      $('html').animate({
+        scrollTop: slider
+      }, 500)
+  })
+
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  $.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+
+  $(window).on('resize scroll', function() {
+    if ($('.animate-left').each(function() {
+      if($(this).isInViewport()) {
+        $(this).addClass('animate__animated animate__fadeInLeft');
+      }
+    }));
+
+    if ($('.animate-right').each(function() {
+      if($(this).isInViewport()) {
+        $(this).addClass('animate__animated animate__fadeInRight');
+      }
+    }));
+
+    if ($('.animate-up').each(function() {
+      if($(this).isInViewport()) {
+        $(this).addClass('animate__animated animate__fadeInUp');
+      }
+    }));
+  });
+
+
   $('#album-slider').slick({
     variableWidth: true,
     draggable: false,
     arrows: true,
+    prevArrow: "<i class='fas fa-long-arrow-alt-left slick-prev'></i>",
+    nextArrow: "<i class='fas fa-long-arrow-alt-right slick-next'></i>",
     centerMode: true,
     centerPadding: '80px',
     slidesToShow: 1,
